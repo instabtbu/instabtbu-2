87777c21fc1bcd90897440876b67c859a4b41ed4
杨培文 <yangpeiwen@YPW-MacBook-Pro.local>
2014-12-15 10:32:00 +0800

    Initial Commit

---
+++ instabtbu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  instabtbu
+//
+//  Created by 杨培文 on 14/12/15.
+//  Copyright (c) 2014年 杨培文. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
