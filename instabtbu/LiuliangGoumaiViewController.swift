bc4e0f3e623b7f06c108c882e4a4f5b8816d0e63
ypwhs <915505626@qq.com>
2015-04-09 23:53:03 +0800

    master
    包流量UI

---
+++ instabtbu/LiuliangGoumaiViewController.swift
@@ -0,0 +1,21 @@
+//
+//  LiuliangGoumaiViewController.swift
+//  instabtbu
+//
+//  Created by 杨培文 on 15/4/9.
+//  Copyright (c) 2015年 杨培文. All rights reserved.
+//
+
+import UIKit
+
+class LiuliangGoumaiViewController: UIViewController {
+
+
+    @IBAction func bagb(sender: AnyObject) {
+        println(8)
+        
+    }
+    @IBAction func ershigb(sender: AnyObject) {
+        println(20)
+    }
+}
